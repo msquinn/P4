63d1b5ad34cff2c0d77d6ac95edcf00934fde7ca
kylehagemann25 <kylefhagemann@Kyles-MacBook-Pro.local>
2015-05-05 02:33:53 -0500

    Only option three method left

--- src/AsTheCrowFlies.java
+++ src/AsTheCrowFlies.java
@@ -1,19 +1,22 @@
 import java.util.*;
 import java.io.*;
+
 public class AsTheCrowFlies {
 	private static ArrayList <City> availableCities = new ArrayList<City>();
 	private static Scanner scnr = new Scanner(System.in);
 
 	//Main Method
 	public static void main(String[] args){
+		System.out.println("As The Crow Flies");
+		System.out.println();
 		menu();
 	}
 
-
-
-
+	/**
+	 * Main menu option one.
+	 */
 	private static void optionOne() {
-		System.out.println("Enter the filename:");
+		System.out.print("Enter the filename: ");
 
 		String fileName = scnr.nextLine();
 		File file = new File(fileName);
@@ -22,32 +25,102 @@ public class AsTheCrowFlies {
 			in = new Scanner(file);
 			in.useDelimiter(",");
 		}catch(FileNotFoundException e){
-			System.out.println("Enter Valid File Name");
+			System.out.print("Enter Valid File Name: ");
 			return;
 		}
-		
+
 		int count = 0;
-		while(in.hasNext()){
-			String state = in.next();
-			String cityName = in.next();
-			Double latitude = in.nextDouble();
-			Double longitude = in.nextDouble();
+		while(in.hasNextLine()){
+			String[] temp = in.nextLine().split(",");
+			String state = temp[0];
+			String cityName = temp[1];
+			Double latitude = new Double(temp[2]);
+			Double longitude = new Double(temp[3]);
+
 			City city = new City (state,cityName,latitude,longitude);
 			availableCities.add(city);
 			count++;
 		}
 		System.out.println(count+" cities added");
+		System.out.println();
+		in.close();
+		return;
+	}
+
+	/**
+	 * Main menu option two.
+	 */
+	public static void optionTwo(){
+		for(int i=0; i<availableCities.size(); i++){
+			System.out.println(availableCities.get(i));
+		}
+		System.out.println();
+		return;
+	}
+
+	/**
+	 * Main menu option three.
+	 */
+	public static void optionThree(){
+		//TODO
+		return;
+	}
+
+	/**
+	 * Main menu option four.
+	 */
+	public static void optionFour(){
+		System.out.print("Enter state name: ");
+		String state = scnr.nextLine().toUpperCase();
+
+		System.out.print("Enter city name: ");
+		String cityName = scnr.nextLine().toUpperCase();
+
+		System.out.print("Enter latitude as double (-90.0 to 90.0): ");
+		double lat = scnr.nextDouble();
+
+		System.out.print("Enter longitude as double (-180.0 to 180.0): ");
+		double longi = scnr.nextDouble();
+
+		City city = new City(state, cityName, lat, longi);
+		availableCities.add(city);
+		System.out.println("Added: " + city);
+		System.out.println();
+		return;
+	}
+
+	/**
+	 * Main menu option five.
+	 */
+	public static void optionFive(){
+		System.out.println("Thank you for your business.");
+		System.out.print("Saved available cities to available_cities.txt");
+
+		PrintWriter writer = null;
+		try{
+			writer = new PrintWriter("available_cities.txt");
+		} catch(FileNotFoundException e) {
+			System.out.println("Print writer error");
+			return;
+		}
+		
+		for(int i=0; i<availableCities.size(); i++){
+			writer.println(availableCities.get(i));
+		}
+		
+		writer.close();
+		return;
 	}
 
 	private static void menu() {
 		int results = -1;
 		do{
-			System.out.println("1. Load available cities from a file"+"\n"+
+			System.out.print("1. Load available cities from a file"+"\n"+
 					"2. Display available cities"+"\n"+
 					"3. Create a trip"+"\n"+
 					"4. Add a city to available cities"+"\n"+
 					"5. Exit Program"+"\n"+
-					"Enter choice as integer [1-5]:");
+					"Enter choice as integer [1-5]: ");
 			String input = scnr.nextLine();
 			results = -1;
 			switch(input){
@@ -57,19 +130,19 @@ public class AsTheCrowFlies {
 				break;
 			case"2":
 				results = 2;
-				System.out.println("2");
+				optionTwo();
 				break;
 			case"3":
 				results = 3;
-				System.out.println("3");
+				optionThree();
 				break;
 			case"4":
 				results = 4;
-				System.out.println("4");
+				optionFour();
 				break;
 			case"5":
 				results = 5;
-				System.out.println("5");
+				optionFive();
 				break;
 			default:
 				System.out.println("'" + input + "'" + " " + "is not a valid "
--- src/City.java
+++ src/City.java
@@ -1,53 +1,53 @@
 
 public class City {
-//instance variable
-private	String state;
-private String name;
-private double latitude;
-private double longitude;
-	
-	
+	//instance variable
+	private	String state;
+	private String name;
+	private double latitude;
+	private double longitude;
+
+
 	//constructor
 	public City(String state, String name, double latitude, double longitude ){
-	this.state = state;
-	this.name = name;
-	this.latitude = latitude;
-	this.longitude = longitude;
-}
+		this.state = state;
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
 	public String getState(){
 		return this.state;
 	}
 	public void setState(String state){
 		this.state = state;
 	}
-	
+
 	public String getName(){
 		return this.name;
 	}
+	
 	public void setName(String name){
 		this.name = name;
 	}
+	
 	public double getLatitude(){
 		return this.latitude;
 	}
+	
 	public void setLatitude(double latitude){
 		this.latitude  = latitude;
 	}
+	
 	public double getLongitude(){
 		return this.longitude;
 	}
+	
 	public void setLongitude(double longitude){
 		this.longitude = longitude;
 	}
+	
 	@Override 
 	public String toString(){
-		return state.toUpperCase() + ", "+ name.toUpperCase() + ", "+
-	latitude + ", "+ longitude;
-		
+		return state.toUpperCase() + ","+ name.toUpperCase() + ","+
+				latitude + ","+ longitude;
 	}
-	
-	
-	
-	
-	
 }
