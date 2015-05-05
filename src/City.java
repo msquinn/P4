c36debdac5a0b77c01e8e98c484415944bbe5ddd
msquinn <msquinn1@yahoo.com>
2015-05-04 22:50:46 -0500

    City class

--- src/City.java
+++ src/City.java
@@ -1,4 +1,28 @@
 
 public class City {
-
+//instance variable
+private	String state;
+private String name;
+private double latitude;
+private double longitude;
+	
+	
+	//constructor
+	public City(String state, String name, double latitude, double longitude ){
+	this.state = state;
+	this.name = name;
+	this.latitude = latitude;
+	this.longitude = longitude;
+}
+	@Override 
+	public String toString(){
+		return state.toUpperCase() + ", "+ name.toUpperCase() + ", "+
+	latitude + ", "+ longitude;
+		
+	}
+	
+	
+	
+	
+	
 }
