db0bb1bd7245ca85a3f3f538c2493b61f32a7179
AdamConverse <adamconverse@Adams-MacBook-Pro.local>
2015-05-04 23:28:56 -0500

    Trip class

--- src/Trip.java
+++ src/Trip.java
@@ -1,4 +1,44 @@
 
 public class Trip {
-
+	/* Trip list */
+	ArrayList<City> cities = null;
+	
+	/**
+	 * Cities constructor
+	 * @param cities A list of cities in order of visiting.
+	 */
+	public Trip(ArrayList<City> cities){
+		this.cities = new ArrayList<City>;
+		this.cities.addAll(cities);
+	}
+	
+	/**
+	 * Cities constructor
+	 */
+	public Trip(){
+		this.cities = new ArrayList<City>;
+	}
+	
+	/**
+	 * Adds city to list.
+	 * @param City City to add.
+	 */
+	public void addCity(City city){
+		this.cities.add(city);
+	}
+	
+	/**
+	 * Returns distance between two cities.
+	 * @param c1 City one
+	 * @param c2 City two
+	 */
+	public int getDistance(City c1, City c2){
+		
+	}
+	
+	@Override
+	public String toString(){
+		String s = "";
+		
+	}
 }
