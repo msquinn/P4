652aeb70d15e3d5b16cef656c2de15ccac21d2f4
msquinn <msquinn1@yahoo.com>
2015-05-04 23:03:14 -0500

    city class complete

--- src/City.java
+++ src/City.java
@@ -14,6 +14,31 @@ private double longitude;
 	this.latitude = latitude;
 	this.longitude = longitude;
 }
+	public String getState(){
+		return this.state;
+	}
+	public void setState(String state){
+		this.state = state;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public double getLatitude(){
+		return this.latitude;
+	}
+	public void setLatitude(double latitude){
+		this.latitude  = latitude;
+	}
+	public double getLongitude(){
+		return this.longitude;
+	}
+	public void setLongitude(double longitude){
+		this.longitude = longitude;
+	}
 	@Override 
 	public String toString(){
 		return state.toUpperCase() + ", "+ name.toUpperCase() + ", "+
